c448ace1d6c4dea732ce50ed67aa80b784542972
boolfalse <boolfalse@mail.ru>
2022-08-14 15:34:30 +0400

    Express error handling

--- backend/controllers/goals.js
+++ backend/controllers/goals.js
@@ -7,6 +7,13 @@ module.exports = {
         });
     },
     create: (req, res) => {
+        if (!req.body.text) {
+            return res.status(400).json({
+                success: false,
+                message: 'Text is required!'
+            });
+        }
+
         return res.status(200).json({
             success: true,
             message: 'New Goal created successfully.'
--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,34 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+
 app.get('/api', (req, res) => {
     return res.status(200).json({
         success: true,
         message: 'Root URL'
     });
 });
-// routes for /api/goal
 app.use('/api/goals', require('./routes/goals'));
+
+
+
+// Handling errors for 404 not-founds
+app.use((req, res) => {
+    return res.status(404).json({
+        success: false,
+        message: "URL-endpoint not found!"
+    });
+});
+// Handling errors for any other cases from whole application
+app.use((err, req, res) => {
+    return res.status(500).json({
+        err_number: 125,
+        error: "Something went wrong!"
+    });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
