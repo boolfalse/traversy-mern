ee88fcb4362f778e512a2651b48ebae2c589886e
boolfalse <boolfalse@mail.ru>
2022-08-18 00:46:07 +0400

    Login & Register pages initials

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,53 @@
 
+import {useState} from "react";
+import {FaSignInAlt} from "react-icons/fa";
+
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+    const { email, password } = formData;
+
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log(formData);
+    }
+
     return (
-        <div>
-            <h2>Login</h2>
-        </div>
+        <>
+            <section className='heading'>
+                <FaSignInAlt /> Login
+                <p>Sign in to your account</p>
+            </section>
+            <section className='form'>
+                <form onSubmit={(e) => onSubmit(e)}>
+                    <div className='form-group'>
+                        <input onChange={(e) => setFormData({...formData, email: e.target.value})}
+                               type='email'
+                               id='email'
+                               name='email'
+                               value={email}
+                               placeholder='Email'
+                               autoComplete='off'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input onChange={(e) => setFormData({...formData, password: e.target.value})}
+                               type='password'
+                               id='password'
+                               name='password'
+                               value={password}
+                               placeholder='Password'
+                               autoComplete='off'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <button type='submit' className='btn btn-block'>Login</button>
+                    </div>
+                </form>
+            </section>
+        </>
     );
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,75 @@
 
+import {useState} from "react";
+import {FaUser} from "react-icons/fa";
+
 const Register = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirm_password: '',
+    });
+    const { name, email, password, confirm_password } = formData;
+
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log(formData);
+    }
+
     return (
-        <div>
-            <h2>Register</h2>
-        </div>
+        <>
+            <section className='heading'>
+                <FaUser /> Register
+                <p>Create an account</p>
+            </section>
+            <section className='form'>
+                <form onSubmit={(e) => onSubmit(e)}>
+                    <div className='form-group'>
+                        <input onChange={(e) => setFormData({...formData, name: e.target.value})}
+                               type='text'
+                               id='name'
+                               name='name'
+                               value={name}
+                               placeholder='Name'
+                               autoComplete='off'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input onChange={(e) => setFormData({...formData, email: e.target.value})}
+                               type='email'
+                               id='email'
+                               name='email'
+                               value={email}
+                               placeholder='Email'
+                               autoComplete='off'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input onChange={(e) => setFormData({...formData, password: e.target.value})}
+                               type='password'
+                               id='password'
+                               name='password'
+                               value={password}
+                               placeholder='Password'
+                               autoComplete='off'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input onChange={(e) => setFormData({...formData, confirm_password: e.target.value})}
+                               type='password'
+                               id='confirm_password'
+                               name='confirm_password'
+                               value={confirm_password}
+                               placeholder='Confirm Password'
+                               autoComplete='off'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <button type='submit' className='btn btn-block'>Register</button>
+                    </div>
+                </form>
+            </section>
+        </>
     );
 }
 
